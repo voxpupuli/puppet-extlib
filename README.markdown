977744d6d05ed5801a20aa9672a80cced1092d00
Igor Galić <i.galic@brainsware.org>
2014-09-03 00:24:12 +0200

    migrate pr to stdlib into standalone puppet module
    this module is a standalone implementation of
    
       https://github.com/puppetlabs/puppetlabs-stdlib/pull/309
    
    a function which can be used before create_resources to deep_merge()
    hashes, supplying them with defaults.

---
+++ README.markdown
@@ -0,0 +1,88 @@
+####Table of Contents
+
+1. [Overview](#overview)
+3. [Setup - The basics of getting started with resources_deep_merge](#setup)
+4. [Usage - Configuration options and additional functionality](#usage)
+5. [Limitations - OS compatibility, etc.](#limitations)
+6. [Development - Guide for contributing to the module](#development)
+
+##Overview
+
+This module provides the function `resources_deep_merge`. It's based on
+[stdlib](https://github.com/puppetlabs/puppetlabs-stdlib) `deep_merge()`
+function. Its intended use to provide hashes, such as those passed to
+`create_resources()` with default values.
+
+##Setup
+
+```console
+ % puppet module install brainsware-resources_deep_merge
+```
+
+##Usage
+
+resources_deep_merge
+--------------------
+
+Returns a [deep-merged](#deep_merge) resource hash (hash of hashes).
+
+*Examples:*
+
+    $tcresource_defaults = {
+      ensure     => 'present',
+      attributes => {
+        driverClassName => 'org.postgresql.Driver',
+      }
+    }
+
+    $tcresources = {
+      'app1:jdbc/db1' => {
+        attributes => {
+          url      => 'jdbc:postgresql://localhost:5432/db1',
+          userpass => 'user1:pass1',
+        },
+      },
+      'app2:jdbc/db2' => {
+        attributes => {
+          url      => 'jdbc:postgresql://localhost:5432/db2',
+          userpass => 'user2:pass2',
+        },
+      }
+    }
+
+When called as:
+
+    $result = resources_deep_merge($tcresources, $tcresource_defaults)
+
+will return:
+    {
+     'app1:jdbc/db1' => {
+       ensure     => 'present',
+       attributes => {
+         url      => 'jdbc:postgresql://localhost:5432/db1',
+         userpass => 'user1:pass1',
+         driverClassName => 'org.postgresql.Driver',
+       },
+     },
+     'app2:jdbc/db2' => {
+       ensure     => 'present',
+       attributes => {
+         url      => 'jdbc:postgresql://localhost:5432/db2',
+         userpass => 'user2:pass2',
+         driverClassName => 'org.postgresql.Driver',
+       },
+     }
+    }
+
+- *Type*: rvalue
+
+##Limitations
+
+This module requires puppetlabs-stdlib >=3.2.1, which is when `deep_merge()`
+was introduced.
+
+##Development
+
+This module uses blacksmith for releasing and rspec for tests.
+All contributions are highly welcome, especially if they include documentation
+and tests.
---
+++ spec/unit/puppet/parser/functions/resources_deep_merge_spec.rb
@@ -0,0 +1,67 @@
+#! /usr/bin/env ruby -S rspec
+
+#  Copyright 2014 Brainsware
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+require 'spec_helper'
+
+describe Puppet::Parser::Functions.function(:resources_deep_merge) do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
+  it "should exist" do
+    expect(Puppet::Parser::Functions.function("resources_deep_merge")).to eq("function_resources_deep_merge")
+  end
+
+  it "should raise an ArgumentError if there is less than 1 arguments" do
+    expect { scope.function_resources_deep_merge([]) }.to( raise_error(ArgumentError))
+  end
+
+  it "should not compile when 1 argument is passed" do
+    my_hash={'one' => 1}
+    expect { scope.function_resources_deep_merge([my_hash]) }.to( raise_error(ArgumentError))
+  end
+
+  it 'should require all parameters are hashes' do
+    expect { scope.function_resources_deep_merge([{}, '2']) }.to( raise_error(ArgumentError))
+  end
+
+  describe 'when calling resources_deep_merge on a resource and a defaults hash' do
+    it 'should be able to deep_merge a resource hash and a defaults hash' do
+      resources = {
+        'one' => {
+          'attributes' => {
+            'user' => '1',
+            'pass' => '1'
+          }
+        },
+        'two' => {
+          'attributes' => {
+            'user' => '2',
+            'pass' => '2'
+          }
+        }
+      }
+      defaults = {
+        'ensure' => 'present',
+        'attributes' => {
+          'type' => 'psql'
+        }
+      }
+      result = scope.function_resources_deep_merge([resources, defaults])
+      result['one'].should == {'ensure' => 'present', 'attributes' => {'type'=>'psql', 'user' => '1', 'pass' => '1'}}
+      result['two'].should == {'ensure' => 'present', 'attributes' => {'type'=>'psql', 'user' => '2', 'pass' => '2'}}
+    end
+  end
+
+end
