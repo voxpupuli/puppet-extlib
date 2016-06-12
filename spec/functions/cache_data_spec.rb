febc99f991ef4697da9bafada08f977359a6dc48
Tim Meusel <tim@bastelfreak.de>
2016-06-12 17:18:34 +0200

    set correct encoding in spec test for puppet4
    this fixes:
    https://travis-ci.org/voxpupuli/puppet-extlib/jobs/137069383#L709

--- spec/functions/cache_data_spec.rb
+++ spec/functions/cache_data_spec.rb
@@ -17,7 +17,8 @@ describe 'cache_data' do
 
   context 'when data already exists' do
     before do
-      File.stubs(:read).with(regexp_matches(%r{metadata.json})).returns('{}')
+      File.stubs(:read).with(regexp_matches(%r{metadata.json}), encoding: 'utf-8').returns('{}').at_least(0)
+      File.stubs(:read).with(regexp_matches(%r{metadata.json})).returns('{}').at_least(0)
       File.stubs(:exists?).with(regexp_matches(%r{#{filename}})).returns(true)
       File.expects(:read).with(regexp_matches(%r{#{filename}})).returns(initial_data).once
     end
