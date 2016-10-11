52888b7e79ec7391a50e80a96ef2bf300349baa1
Tim Meusel <tim@bastelfreak.de>
2016-10-11 17:25:43 +0200

    rubocop: fix RSpec/MessageExpectation

--- spec/unit/puppet/parser/functions/echo_spec.rb
+++ spec/unit/puppet/parser/functions/echo_spec.rb
@@ -12,27 +12,27 @@ describe 'echo', type: :puppet_function do
 
   context 'should output a correct debug string for' do
     it 'a string' do
-      expect(scope).to receive(:debug).with '(String) "test"'
+      allow(scope).to receive(:debug).with '(String) "test"'
       is_expected.to run.with_params('test')
     end
     it 'a string with a comment' do
-      expect(scope).to receive(:debug).with 'My String (String) "test"'
+      allow(scope).to receive(:debug).with 'My String (String) "test"'
       is_expected.to run.with_params('test', 'My String')
     end
     it 'and array' do
-      expect(scope).to receive(:debug).with 'My Array (Array) ["1", "2", "3"]'
+      allow(scope).to receive(:debug).with 'My Array (Array) ["1", "2", "3"]'
       is_expected.to run.with_params(%w(1 2 3), 'My Array')
     end
     it 'a hash' do
-      expect(scope).to receive(:debug).with 'My Hash (Hash) {"a"=>"1"}'
+      allow(scope).to receive(:debug).with 'My Hash (Hash) {"a"=>"1"}'
       is_expected.to run.with_params({ 'a' => '1' }, 'My Hash')
     end
     it 'a boolean value' do
-      expect(scope).to receive(:debug).with 'My Boolean (FalseClass) false'
+      allow(scope).to receive(:debug).with 'My Boolean (FalseClass) false'
       is_expected.to run.with_params(false, 'My Boolean')
     end
     it 'an undefind value' do
-      expect(scope).to receive(:debug).with 'Undefined Value (NilClass) nil'
+      allow(scope).to receive(:debug).with 'Undefined Value (NilClass) nil'
       is_expected.to run.with_params(nil, 'Undefined Value')
     end
   end
