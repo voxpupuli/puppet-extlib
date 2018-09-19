be5fdbd8180e0b37f02f99ac79348fa10d8744c8
Alexander Fisher <alex@linfratech.co.uk>
2018-09-19 22:15:56 +0100

    Add deprecated non-namespaced functions back
    Not including `has_module` and `sort_by_version` as these never had non
    namespaced versions.

---
+++ spec/functions/echo_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'echo' do
+  describe 'signature validation' do
+    it 'exists' do
+      is_expected.not_to be_nil
+    end
+    it 'requires at least one argument' do
+      is_expected.to run.with_params.and_raise_error(ArgumentError)
+    end
+  end
+
+  context 'should output a correct debug string for' do
+    it 'a string' do
+      allow(scope).to receive(:debug).with '(String) "test"'
+      is_expected.to run.with_params('test')
+    end
+    it 'a string with a comment' do
+      allow(scope).to receive(:debug).with 'My String (String) "test"'
+      is_expected.to run.with_params('test', 'My String')
+    end
+    it 'and array' do
+      allow(scope).to receive(:debug).with 'My Array (Array) ["1", "2", "3"]'
+      is_expected.to run.with_params(%w[1 2 3], 'My Array')
+    end
+    it 'a hash' do
+      allow(scope).to receive(:debug).with 'My Hash (Hash) {"a"=>"1"}'
+      is_expected.to run.with_params({ 'a' => '1' }, 'My Hash')
+    end
+    it 'a boolean value' do
+      allow(scope).to receive(:debug).with 'My Boolean (FalseClass) false'
+      is_expected.to run.with_params(false, 'My Boolean')
+    end
+    it 'an undefind value' do
+      allow(scope).to receive(:debug).with 'Undefined Value (NilClass) nil'
+      is_expected.to run.with_params(nil, 'Undefined Value')
+    end
+  end
+end
---
+++ spec/functions/resources_deep_merge_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe 'resources_deep_merge' do
+  let(:resources) do
+    {
+      'one' => {
+        'attributes' => {
+          'user' => '1',
+          'pass' => '1'
+        }
+      },
+      'two' => {
+        'attributes' => {
+          'user' => '2',
+          'pass' => '2'
+        }
+      }
+    }
+  end
+
+  let(:defaults) do
+    {
+      'ensure' => 'present',
+      'attributes' => {
+        'type' => 'psql'
+      }
+    }
+  end
+
+  let(:result) do
+    {
+      'one' => {
+        'ensure' => 'present',
+        'attributes' => {
+          'type' => 'psql',
+          'user' => '1',
+          'pass' => '1'
+        }
+      },
+      'two' => {
+        'ensure' => 'present',
+        'attributes' => {
+          'type' => 'psql',
+          'user' => '2',
+          'pass' => '2'
+        }
+      }
+    }
+  end
+
+  describe 'signature validation' do
+    it 'exists' do
+      is_expected.not_to be_nil
+    end
+
+    it 'raises an ArgumentError if there is less than 1 arguments' do
+      is_expected.to run.with_params.and_raise_error ArgumentError
+    end
+
+    it 'does not compile when 1 argument is passed' do
+      my_hash = { 'one' => 1 }
+      is_expected.to run.with_params(my_hash).and_raise_error ArgumentError
+    end
+
+    it 'requires all parameters are hashes' do
+      is_expected.to run.with_params({}, '2').and_raise_error ArgumentError
+    end
+  end
+
+  describe 'when calling resources_deep_merge on a resource and a defaults hash' do
+    it 'is able to deep_merge a resource hash and a defaults hash' do
+      is_expected.to run.with_params(resources, defaults).and_return(result)
+    end
+  end
+end
