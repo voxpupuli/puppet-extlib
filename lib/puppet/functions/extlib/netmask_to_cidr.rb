5c58755dc1092e5d0a87ceb579b952fe6b9d2f9f
Thore Bödecker <me@foxxx0.de>
2021-10-18 21:36:43 +0200

    add netmask_to_cidr function

---
+++ lib/puppet/functions/extlib/netmask_to_cidr.rb
@@ -0,0 +1,18 @@
+# @summary Converts an octet netmask address of the form 255.255.255.0 into its CIDR variant.
+#          Thus making it directly usable with the values from facter.
+#
+Puppet::Functions.create_function(:'extlib::netmask_to_cidr') do
+  # @param netmask IPv6 or IPv4 netmask in octet notation
+  # @return CIDR / prefix length
+  # @example calling the function
+  #   extlib::netmask_to_cidr('255.0.0.0')
+  dispatch :netmask_to_cidr do
+    param 'Stdlib::IP::Address::Nosubnet', :netmask
+    return_type 'Integer[0, 128]'
+  end
+
+  def netmask_to_cidr(netmask)
+    dummy_addr = IPAddr.new(netmask).ipv6? ? '::' : '0.0.0.0'
+    IPAddr.new("#{dummy_addr}/#{netmask}").prefix
+  end
+end
